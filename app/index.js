a6e22a576d02f990fe1b5a8735ef503daf15f8f9
vogtn <nicolasjvogt@gmail.com>
2016-09-28 12:22:17 -0700

    creating profile link

--- app/index.js
+++ app/index.js
@@ -9,42 +9,53 @@ var ReactDOM = require('react-dom');
 
 var ProfilePic = React.createClass({
   render: function () {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
+  }
+});
+
+var Link = React.createClass({
+  changeURL: function () {
+    window.location.replace(this.props.href)
+  },
+  render: function () {
+    return(
+      <span style={{color: 'blue', cursor: 'pointer'}}
+      onClick={this.changeURL}>
+        {this.props.children}
+      </span>
+    )
   }
 });
 
 var ProfileLink = React.createClass({
-  render: function(){
+  render: function () {
     return (
       <div>
-        <Link href={'https://www.github.com/' + this.props.username}>
-        {this.props.username}
-        </Link>
+        <a href={'https://www.github.com/' +  this.props.username}>
+          {this.props.username}
+        </a>
       </div>
-    )
+    );
   }
 });
 
 var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    )
+  render: function (){
+    return <div>{this.props.name}</div>
   }
-})
+});
 
 var Avatar = React.createClass({
-  render: function(){
-    return (
+  render: function () {
+    return(
       <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
       </div>
-    )
+    );
   }
-})
-
+});
 
 
 ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
