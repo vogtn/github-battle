e0a0389d117cfadddb235aac9ed84b856a350d38
vogtn <nicolasjvogt@gmail.com>
2016-09-29 10:53:32 -0700

    separating business logic from presentation compontent

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,28 +1,44 @@
 var React = require('react');
 var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      username: ''
+    }
+  },
+  onUpdateUser: function (e) {
+    this.setState({
+      username: e.target.value
+    })
+  },
+  onSubmitUser: function (e){
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
+    if(this.props.routeParams.playerOne){
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParamas.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+      //go to /battle
+    }else {
+      //go to /playerTwo
+      this.conext.router.push('/playerTwo' + this.state.username)
+    }
+  },
   render: function () {
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h1>{this.props.route.header}</h1>
-        <div className="col-sm-12">
-          <form>
-            <div className="form-group">
-              <input
-                className="form-control"
-                placeholder="Github Username"
-                type="text" />
-            </div>
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button className="btn btn-block btn-success"
-                type="submit">
-                Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt />
     )
   }
 });
