267fef08c15148c916bbc8b3f2746d7e736529f0
vogtn <nicolasjvogt@gmail.com>
2016-09-28 19:41:10 -0700

    adding landing page edits

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,15 +3,19 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
+var PromptContainer = require('../containers/PromptContainer');
 
 //SET MAIN ROUTE
 //Main will always be active
 var routes = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <Route path='/home' component={Home} />
+      <IndexRoute component={Home} />
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
     </Route>
   </Router>
 );
--- app/index.js
+++ app/index.js
@@ -1,6 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
+var Raven = require('raven-js');
+
+var sentryKey = '92852642c6c645c08526a0be921d7a48'
+var sentryApp = '102313'
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp
+
+var _APP_INFO = {
+  name: 'Github Battle',
+  branch: 'video4',
+  version: '1.0'
+}
+
+Raven.config(sentryURL, {
+  release: _APP_INFO.version,
+  tags: {
+    branch: _APP_INFO.branch,
+  }
+}).install();
 
 
 ReactDOM.render(routes, document.getElementById('app'));
